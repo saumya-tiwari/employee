3488d3c05568846c86c9448f47d37109bc7c8e66
Saumya Tiwari <taru.saumya@gmail.com>
2020-07-18 13:36:42 +0530

    updated sql file

--- src/main/resources/employeeData.sql
+++ src/main/resources/employeeData.sql
@@ -1,1 +1,5 @@
 db.employee.insert( { id: "10001", name: "Saumya",age:30 } )
+
+db.employee.insert( { id: "10002", name: "Nilesh",age:37 } )
+
+db.employee.insert( { id: "10003", name: "Nitya",age:22 } )
